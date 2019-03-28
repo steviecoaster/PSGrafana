b97bfd995d7d65035667011d5be8f9849329f72e
Stephen Valdinger <stephen@chocolatey.io>
2019-03-28 19:24:46 -0400

    Pester scope

--- Build/build.ps1
+++ Build/build.ps1
@@ -72,7 +72,7 @@ if($Test.IsPresent) {
         throw "Cannot find the 'Pester' module. Please specify '-Bootstrap' to install build dependencies."
     }
 
-    Install-Module -Name Pester -RequiredVersion 4.3.1
+    Install-Module -Name Pester -RequiredVersion 4.3.1 -Scope CurrentUser
 
     $RelevantFiles = (Get-ChildItem $PSScriptRoot -Recurse -Include "*.psm1","*.ps1").FullName
 
