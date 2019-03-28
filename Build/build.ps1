0f9767bf3b7c3914b2a5916adeccbadfa0abdb44
Stephen Valdinger <stephen@chocolatey.io>
2019-03-28 19:00:24 -0400

    Pester is falling over. Slowing gonna step through it.

--- Build/build.ps1
+++ Build/build.ps1
@@ -20,10 +20,7 @@ if ($Bootstrap.IsPresent) {
         Install-Module -Name Pester -Scope CurrentUser -Force
     }
 
-    if (-not (Get-Module -Name PSCodeCovIo -ListAvailable)) {
-        Write-Warning "Module 'PSCodeCovIo' is missing. Installing 'PSCodeCovIo' ..."
-        Install-Module -Name PSCodeCovIo -Scope CurrentUser -Force
-    }
+
 }
 
 # Compile step
@@ -64,7 +61,7 @@ if ($Compile.IsPresent) {
     Compress-Archive -Path .\PSGrafana\* -DestinationPath .\Azure-Pipelines\PSGrafana.zip
 
     # Re-import module, extract release notes and version
-    Import-Module ./PSGrafana/PSGrafana.psd1 -Force
+    Import-Module .\PSGrafana.psd1 -Force
     (Get-Module PSGrafana)[0].ReleaseNotes | Add-Content .\Azure-Pipelines\release-notes.txt
     (Get-Module PSGrafana)[0].Version.ToString() | Add-Content .\Azure-Pipelines\release-version.txt
 }
@@ -75,19 +72,17 @@ if($Test.IsPresent) {
         throw "Cannot find the 'Pester' module. Please specify '-Bootstrap' to install build dependencies."
     }
 
-    if (-not (Get-Module -Name PSCodeCovIo -ListAvailable)) {
-        throw "Cannot find the 'PSCodeCovIo' module. Please specify '-Bootstrap' to install build dependencies."
-    }
+    
 
-    $RelevantFiles = (Get-ChildItem ./PSGrafana -Recurse -Include "*.psm1","*.ps1").FullName
+    $RelevantFiles = (Get-ChildItem $PSScriptRoot -Recurse -Include "*.psm1","*.ps1").FullName
 
-    $RelevantFiles = (Get-ChildItem ./PSGrafana -Recurse -Include "*.psm1","*.ps1").FullName
+    $RelevantFiles = (Get-ChildItem $PSScriptRoot -Recurse -Include "*.psm1","*.ps1").FullName
 
     if ($env:TF_BUILD) {
-        $res = Invoke-Pester "./Tests" -OutputFormat NUnitXml -OutputFile TestResults.xml -CodeCoverage $RelevantFiles -PassThru
+        $res = Invoke-Pester "./Test" -OutputFormat NUnitXml -OutputFile TestResults.xml -CodeCoverage $RelevantFiles -PassThru
         if ($res.FailedCount -gt 0) { throw "$($res.FailedCount) tests failed." }
     } else {
-        $res = Invoke-Pester "./Tests" -CodeCoverage $RelevantFiles -PassThru
+        $res = Invoke-Pester "./Tests" -PassThru
     }
 
 }
