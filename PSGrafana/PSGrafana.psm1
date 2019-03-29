f26c5f5bb9502c730cccebd096d6389aadc3e75f
Stephen Valdinger <stephen@chocolatey.io>
2019-03-28 20:38:11 -0400

    More paths

--- PSGrafana/PSGrafana.psm1
+++ PSGrafana/PSGrafana.psm1
@@ -6,27 +6,3 @@ Foreach($Script in @($public + $private)){
     . $Script.FullName
 
 }
-<#
-If(!(Test-Path $PSScriptRoot\Config\Grafana.json)){
-    $grafanaConfig = @{
-        
-    }
-    Write-Host "No config file found in Config folder. Assuming first run..." -ForegroundColor yellow
-    Write-Host "We will now ask some questions to get things setup" -ForegroundColor yellow
-    $GrafanaUri = Read-Host -Prompt "What is your base Grafana uri?"
-
-    Write-Host "Adding URI and appending /api to config file..." -ForegroundColor yellow
-    $grafanaConfig.Add('GrafanaUri',"$GrafanaUri/api")
-
-    $ApiKey = Read-Host -Prompt "What is your API Key? Found at https://$GrafanaUri/org/apikeys"
-
-    Write-Host "Adding API Key to config file..." -ForegroundColor yellow
-    $grafanaConfig.Add('apikey',$ApiKey)
-
-    $grafanaConfig | ConvertTo-Json | Out-File $PSScriptRoot\Config\Grafana.json
-
-    Write-Host "Config file has been generated successfully. Run 'Get-GrafanaConfig' to verify" -ForegroundColor yellow
-    
-
-}
-#>
